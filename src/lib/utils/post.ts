2f999696a3b6718359a0bdb8848b3cb2d53f8a0c
MineokaYuto <19fi112@ms.dendai.ac.jp>
2023-10-08 02:23:49 +0900

    Create util files.

---
+++ src/lib/utils/post.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+const post_process = {
+	async get_all_posts(): Promise<any> {
+		try {
+			const res = await prisma.posts.findMany()
+			return res
+		} catch (e: unknown) {
+			if (e instanceof Error) {
+				console.error(e.message);
+			}
+		}
+	},
+}
+export default post_process
