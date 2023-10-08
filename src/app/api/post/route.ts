08fcce8ae7a0b1a867a255d4337f568d9ffdeaa2
rinbk201 <yamarin8zoo@gmail.com>
2023-10-09 01:14:00 +0900

    add:post投稿用のAPIの作成

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -14,9 +14,45 @@ export const GET = async (req: Request, res: NextResponse) => {
   try {
     await main();
     const posts = await prisma.post.findMany();
-    return NextResponse.json({message: "Success", posts},{status: 200});
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
-    return NextResponse.json({message: "Error", err},{status:500});
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//post投稿用のAPI
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+    const {
+      userInfoId,
+      title,
+      launchDay,
+      deadline,
+      recruitmentNumbers,
+      description,
+      matchingMessage,
+      requirementLanguages,
+      modifiedAt,
+    } = await req.json();
+    await main();
+    const post = await prisma.post.create({
+      data: {
+        userInfoId,
+        title,
+        launchDay,
+        deadline,
+        recruitmentNumbers,
+        description,
+        matchingMessage,
+        requirementLanguages,
+        modifiedAt,
+      },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   } finally {
     await prisma.$disconnect();
   }
