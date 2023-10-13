78baf5ae5a027f085123a55874efc95284d1354f
MineokaYuto <19fi112@ms.dendai.ac.jp>
2023-10-14 00:27:23 +0900

    戻り値を変更

--- src/app/api/user/[id]/language/route.ts
+++ src/app/api/user/[id]/language/route.ts
@@ -11,16 +11,15 @@ export const GET = async (req: Request, res: NextResponse) => {
     const user_info_id: number = parseInt(req.url.split("/user/")[1]);
     await main();
     const languages_S: Record<string, any> | null = await prisma.programmingLanguages.findMany({ where: { userInfoId: user_info_id }, include: { language: true} });
-    var languages = {} as Record<string, string | number| undefined>;
-    const language_N = []
+    const languages = []
     for (const i in languages_S) {
       const language = {
         "languageName": languages_S[i].language.languageName,
         "level": level_string_to_int(languages_S[i].level)
       }
-      language_N.push(language)
+      languages.push(language)
     }
-    return NextResponse.json({ message: "Success", language_N }, { status: 200 });
+    return NextResponse.json({ message: "Success", languages }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   } finally {
--- src/app/api/user/[id]/post/route.ts
+++ src/app/api/user/[id]/post/route.ts
@@ -7,34 +7,34 @@ const prisma = new PrismaClient();
 //Userに関連する全てのPOST情報を取得
 export const GET = async (req: Request, res: NextResponse) => {
   try {
-    const data: {[key: string]: Array<any>} = {"APPROVED": [], "UNAPPROVED": [], "CONFIRMED": [], "BOOKMARK": []}
+    const posts: {[key: string]: Array<any>} = {"APPROVED": [], "UNAPPROVED": [], "CONFIRMED": [], "BOOKMARK": []}
     const id: number = parseInt(req.url.split("/user/")[1]);
     await main();
-    const posts = await prisma.userPostRelationship.findMany({
+    const post_list = await prisma.userPostRelationship.findMany({
       where: { userInfoId: id },
       include: {
         post: true,
       }
     });
-    for (var i in posts) {
-      if (posts[i].relationState === "APPROVED") {
-        if (posts[i].post.confirmed === true) {
-          data["CONFIRMED"].push(posts[i])
+    for (var i in post_list) {
+      if (post_list[i].relationState === "APPROVED") {
+        if (post_list[i].post.confirmed === true) {
+          posts["CONFIRMED"].push(post_list[i])
           continue
         }
-        data["APPROVED"].push(posts[i])
+        posts["APPROVED"].push(post_list[i])
         continue
       }
-      if (posts[i].relationState === "UNAPPROVED") {
-        if (posts[i].applicationLevel === "BOOKMARK") {
-          data["BOOKMARK"].push(posts[i])
+      if (post_list[i].relationState === "UNAPPROVED") {
+        if (post_list[i].applicationLevel === "BOOKMARK") {
+          posts["BOOKMARK"].push(post_list[i])
           continue
         }
-        data["UNAPPROVED"].push(posts[i])
+        posts["UNAPPROVED"].push(post_list[i])
         continue
       }
     }
-    return NextResponse.json({ message: "Success", data }, { status: 200 });
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
     console.log(err)
     return NextResponse.json({ message: "Error", err }, { status: 500 });
