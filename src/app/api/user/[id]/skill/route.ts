62b84d8c9b4809651f79dfa3c3a6cdb70bdb3152
MineokaYuto <19fi112@ms.dendai.ac.jp>
2023-10-13 15:00:57 +0900

    DB修正に関する修正とLEVELのGET・PUTをintに対応

--- src/app/api/user/[id]/skill/route.ts
+++ src/app/api/user/[id]/skill/route.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { main } from "../../route";
+import { level_int_to_string, level_string_to_int } from "@/lib/utils/transfer";
 
 const prisma = new PrismaClient();
 
@@ -9,7 +10,13 @@ export const GET = async (req: Request, res: NextResponse) => {
   try {
     const user_info_id: number = parseInt(req.url.split("/user/")[1]);
     await main();
-    const skills = await prisma.skills.findFirst({ where: { userInfoId: user_info_id } });
+    const skills_S: Record<string, any> | null = await prisma.skills.findFirst({ where: { userInfoId: user_info_id } });
+    const skills = {} as Record<string, number | undefined>;
+    for (const skill in skills_S) {
+      if (skill !== "id" && skill !== "userInfoId") {
+        skills[skill] = level_string_to_int(skills_S[skill]);
+      }
+    }
     return NextResponse.json({ message: "Success", skills }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
@@ -21,16 +28,21 @@ export const GET = async (req: Request, res: NextResponse) => {
 export const PUT = async (req: Request, res: NextResponse) => {
   try {
     const user_info_id: number = parseInt(req.url.split("/user/")[1]);
-    const {
+    var {
       PLANNING,
       PRESENTATION,
       FRONTEND,
       BACKEND,
       DESIGN,
       OTHER
-    } = await req.json();
+    }: Record<string, any> = await req.json();
     await main();
-    console.log(PLANNING)
+    PLANNING = level_int_to_string(PLANNING)
+    PRESENTATION = level_int_to_string(PRESENTATION)
+    FRONTEND = level_int_to_string(FRONTEND)
+    BACKEND = level_int_to_string(BACKEND)
+    DESIGN = level_int_to_string(DESIGN)
+    OTHER = level_int_to_string(OTHER)
     const skills = await prisma.skills.update({
       data: {
         PLANNING,
