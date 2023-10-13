566f8edb7d843aef1e5a095b64caabbff93cffa4
MineokaYuto <19fi112@ms.dendai.ac.jp>
2023-10-13 18:27:53 +0900

    Update.

--- @types/library.d.ts
+++ @types/library.d.ts
@@ -1,26 +1,54 @@
+import { type } from "os";
+
 export type PostType = {
-    id: number;
-    userInfoId: number;
-    title: string;
-    launchDay: Date;
-    deadline:Date;
-    recruitmentNumbers:number;
-    description:string;
-    matchingMessage: string;
-    confirmed: boolean;
-    createdAt:Date;
-    modifiedAt:Date;
-    deletedAt:Date;
-  }
+  id: number;
+  userInfoId: number;
+  title: string;
+  launchDay: Date;
+  deadline: Date;
+  recruitmentNumbers: number;
+  description: string;
+  matchingMessage: string;
+  confirmed: boolean;
+  createdAt: Date;
+  modifiedAt: Date;
+  deletedAt: Date;
+}
 
 export type NewPostType = {
-    userInfoId:number;
-    title:string;
-    launchDay:Date;
-    deadline:Date;
-    recruitmentNumbers:number;
-    description:string|undefined;
-    matchingMessage:string|undefined;
-    requirementLanguages:string|undefined;
-    modifiedAt:Date;
+  userInfoId: number;
+  title: string;
+  launchDay: Date;
+  deadline: Date;
+  recruitmentNumbers: number;
+  description: string | undefined;
+  matchingMessage: string | undefined;
+  requirementLanguages: string | undefined;
+  modifiedAt: Date;
 }
+
+export type EditPostType = {
+  title: string;
+  launchDay: Date;
+  deadline: Date;
+  recruitmentNumbers: number;
+  matchingMessage: string;
+  requirementLanguages: [
+    { "language": string, "level": number }
+  ]
+  modifiedAt: Date;
+}
+
+export type NewUserRelationType = {
+  UserInfoId: number;
+}
+export type ChangeUserRelationStateType = {
+  userInfoId: Number;
+  relationState: string;
+}
+
+export type DeleteUserPostRerationType = {
+  userInfoId: number;
+}
+
+export type 
