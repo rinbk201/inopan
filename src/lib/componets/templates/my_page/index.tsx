aa734c9ac46b9e233f1f8a73339f64bfd5ea77e0
MineokaYuto <19fi112@ms.dendai.ac.jp>
2023-10-14 13:33:49 +0900

    Add mypage tag cpmponets

--- src/lib/componets/templates/my_page/index.tsx
+++
@@ -1,8 +0,0 @@
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-
-export default function MyPage() {
-  return <></>;
-}
