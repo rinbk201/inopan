6d3742144ccace02a4a05c23b002cf126ac06193
rinbk201 <yamarin8zoo@gmail.com>
2023-10-14 10:08:41 +0900

    add:トップページを追加

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export type PostType = {
+  id: number;
+  userInfoId: number;
+  title: string;
+  launchDay: Date;
+  deadline: Date;
+  recruitmentNumbers: number;
+  description: string;
+  matchingMessage: string;
+  confirmed: boolean;
+  createdAt: Date;
+  modifiedAt: Date|undefined;
+  deletedAt: Date;
+};
+
+export type NewPostType = {
+  userInfoId: number;
+  title: string;
+  launchDay: Date;
+  deadline: Date;
+  recruitmentNumbers: number;
+  description: string | undefined;
+  matchingMessage: string | undefined;
+  requirementLanguages: string | undefined;
+  modifiedAt: Date;
+};
