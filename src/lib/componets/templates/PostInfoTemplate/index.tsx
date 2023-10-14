eb6af6cce147b1cc565d4f5c150362ca240e7423
rinbk201 <yamarin8zoo@gmail.com>
2023-10-15 03:16:15 +0900

    prefix:imageネクストに対応

--- src/lib/componets/templates/PostInfoTemplate/index.tsx
+++ src/lib/componets/templates/PostInfoTemplate/index.tsx
@@ -53,7 +53,7 @@ export default function PostInfoTemplate({ postData }: PostInfoTemplateProps) {
 
       <Grid container spacing={4}>
         <Grid item xs={12} sm={6}>
-          <PostImageBox imgSrc="../../../../../test.png"></PostImageBox>
+          <PostImageBox imgSrc="https://source.unsplash.com/random"></PostImageBox>
           <PostDetailBox
             title={postData.PostType.title}
             description={postData.PostType.description}
